ad3b3ec748f0f4e30789e8379b2f242c2ad84816
Divine Namirembe <131985298+divinenamirembe@users.noreply.github.com>
2024-09-05 12:50:53 +0300

    Add files via upload

---
+++ Covid vaccinations August 2024.sql
@@ -0,0 +1,11 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [master].[dbo].[PercentPopulationVaccinated]
+
+
+SELECT *
+FROM PercentPopulationVaccinated
